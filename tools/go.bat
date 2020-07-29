854fb439918d10cf95437fb78fa6752bac56e649
TerryH <terry@tnthughes.net>
2020-07-29 00:31:14 -0700

    Add go (to location) script

---
+++ tools/go.bat
@@ -0,0 +1,30 @@
+@ECHO OFF
+
+IF EXIST "W:\projects\%1" (
+	W:
+	CD "\projects\%1"
+	GOTO :EOF
+)
+
+GOTO %1 2> NUL || (
+	ECHO Unknown location '%1'
+)
+
+:projects
+:proj
+:p
+	W:
+	CD "\projects\"
+	GOTO :EOF
+
+:aliases
+:a
+	W:
+	CD "\aliases\"
+	GOTO :EOF
+
+:tools
+:t
+	W:
+	CD "\tools\"
+	GOTO :EOF
