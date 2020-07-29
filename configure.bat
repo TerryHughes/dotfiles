7436e178070b24cafd18c46103b736421e916363
TerryH <terry@tnthughes.net>
2020-07-28 22:46:51 -0700

    Add dotfiles to configure script

--- configure.bat
+++ configure.bat
@@ -1,2 +1,12 @@
 @ECHO OFF
 SETLOCAL
+
+SET dotfiles=dotfiles
+
+where git > NUL 2> NUL
+IF %ERRORLEVEL%==0 (
+	IF NOT EXIST "%dotfiles%" (
+		ECHO Cloning dotfiles into "%dotfiles%"
+		git clone https://github.com/TerryHughes/dotfiles.git "%dotfiles%" 2> NUL
+	)
+)
