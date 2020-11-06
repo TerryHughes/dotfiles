bdc919116bc6dbf353e8d5618d15bfa77f68d7b7
TerryH <terry@tnthughes.net>
2020-11-06 12:43:26 -0800

    Add Git configuration folder to install script

--- install.bat
+++ install.bat
@@ -9,3 +9,11 @@ IF %ERRORLEVEL%==0 (
 		git clone https://github.com/TerryHughes/dotfiles.git "%dotfiles%" 2> NUL
 	)
 )
+
+
+MKDIR ".config\git" 2> NUL
+IF EXIST "%dotfiles%\configurations\git" (
+	IF NOT EXIST ".config\git" (
+		MKLINK /J ".config\git" "%dotfiles%\configurations\git" > NUL
+	)
+)
