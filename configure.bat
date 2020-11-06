f5be733cb12b07dc58d208860505c51627b10c91
TerryH <terry@tnthughes.net>
2020-11-06 12:28:33 -0800

    Add dotfiles-externaltools to configure script

--- configure.bat
+++ configure.bat
@@ -2,11 +2,17 @@
 SETLOCAL
 
 SET dotfiles=dotfiles
+SET dotfilesExternalTools=dotfiles-externaltools
 
 where git > NUL 2> NUL
 IF %ERRORLEVEL%==0 (
 	IF NOT EXIST "%dotfiles%" (
 		ECHO Cloning dotfiles into "%dotfiles%"
 		git clone https://github.com/TerryHughes/dotfiles.git "%dotfiles%" 2> NUL
+	)
+
+	IF NOT EXIST "%dotfilesExternalTools%" (
+		ECHO Cloning dotfiles-externaltools into "%dotfilesExternalTools%"
+		git clone https://github.com/TerryHughes/dotfiles-externaltools.git "%dotfilesExternalTools%" 2> NUL
 	)
 )
