77473363be080d9ab442a0e9920cc065aeb57418
TerryH <terry@tnthughes.net>
2020-11-13 12:40:31 -0800

    fixup! Add visual studio script

--- tools/vs.bat
+++ tools/vs.bat
@@ -1,5 +1,8 @@
 @ECHO OFF
 
+where cl > NUL 2> NUL
+IF %ERRORLEVEL%==0 GOTO :EOF
+
 IF EXIST "miscellaneous\shells\vs2015.bat" CALL "miscellaneous\shells\vs2015.bat"
 IF EXIST "vs2015.bat" CALL "vs2015.bat"
 where cl > NUL 2> NUL
