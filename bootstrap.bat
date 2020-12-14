3be02605ab05f1ae935e9d114ce2f8f080669958
TerryH <terry@tnthughes.net>
2020-12-14 12:42:43 -0800

    fixup! Add Git to bootstrap script

--- bootstrap.bat
+++ bootstrap.bat
@@ -1,14 +1,14 @@
 @ECHO OFF
 SETLOCAL ENABLEDELAYEDEXPANSION
 
-SET DesiredGitVersion=2.28.0
-SET DesiredGitExe=Git-%DesiredGitVersion%-64-bit.exe
+SET DesiredGitVersion=2.29.2
+SET DesiredGitExe=Git-%DesiredGitVersion%.3-64-bit.exe
 
 where git > NUL 2> NUL
 IF NOT %ERRORLEVEL%==0 (
 	CD "%USERPROFILE%\Downloads"
 	IF NOT EXIST "%DesiredGitExe%" (
-		curl -LJO "https://github.com/git-for-windows/git/releases/download/v%DesiredGitVersion%.windows.1/%DesiredGitExe%"
+		curl -LJO "https://github.com/git-for-windows/git/releases/download/v%DesiredGitVersion%.windows.3/%DesiredGitExe%"
 		IF NOT !ERRORLEVEL!==0 (
 			ECHO curl did not complete successfully: !ERRORLEVEL!
 			GOTO :EOF
