33ce2f8817de285bd6ae60f428440a8e563d8108
TerryH <terry@tnthughes.net>
2023-06-03 07:48:06 -0700

    Remove Microsoft Build Tools 2015 Update 3 from install script
    Microsoft Build Tools 2015 Update 3 doesn't seem to be able to install anymore

--- install.bat
+++ install.bat
@@ -11,8 +11,4 @@ IF NOT %ERRORLEVEL%==0 (
 	CALL "installs\vim.bat"
 )
 
-IF NOT EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" (
-	CALL "installs\microsoft-build-tools-2015-update-3.bat"
-)
-
 EXIT 0
